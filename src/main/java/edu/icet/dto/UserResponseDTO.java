4d4372d2aece7c49058cce82f0bcd27e4539be47
PasiSandeepa <pasisandeepa456@gmail.com>
2026-02-28 00:11:48 +0530

    added UserResponseDTO class

---
+++ src/main/java/edu/icet/dto/UserResponseDTO.java
@@ -0,0 +1,17 @@
+package edu.icet.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@Builder
+public class UserResponseDTO {
+    private Long userid;
+    private String username;
+    private String email;
+    private LocalDateTime  createdAt;
+}
