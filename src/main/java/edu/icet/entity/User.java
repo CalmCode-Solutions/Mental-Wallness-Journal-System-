f5bc539630440e9522a3264c59379ddf4bf2c54f
nisalmissaka <wishwakanisal@gmail.com>
2026-02-27 18:20:52 +0530

    change to User entity class

---
+++ src/main/java/edu/icet/entity/User.java
@@ -0,0 +1,35 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Journal> journals;
+}
+
